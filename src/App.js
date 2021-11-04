b94da80c0600dc3290eeabe73f7f9a913ff4b056
QeNo <moodiiy.98@gmail.com>
2021-11-04 13:54:15 +0200

    Install Axios And Test Api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { Component } from 'react';
 import './assets/css/main.css';
 import Navbar from './components/Navbar';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyAVNeTlBqMx89pciJK2Fxf_sID63XtDUOk&type=video&q=spongebob&part=snippet&maxResults=20`)
+            .then(res => {
+                const persons = res.data;
+                console.log(persons)
+                this.setState({ persons });
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+            </div>
+        )
+    };
 }
 
 export default App;
