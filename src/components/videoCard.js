9a7c3576432bd29ab1873aefeef108bc4f662938
QeNo <moodiiy.98@gmail.com>
2021-11-05 14:26:51 +0200

    Code optimization

--- src/components/videoCard.js
+++ src/components/videoCard.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 
 class Video extends Component {
     render () {
+        const {image, title, channelTitle, publishTime, description} = this.props
         return (
             <div className="video">
                 <div className="image">
-                    <img src={this.props.image} />
+                    <img src={image} />
                 </div>
                 <div className="body">
-                    <h3 className="title">{this.props.title}</h3>
+                    <h3 className="title">{title}</h3>
                     <div className="info">
-                        <span className="channelTitle">{this.props.channelTitle}</span>
-                        <span className="publishTime">{this.props.publishTime}</span>
+                        <span className="channelTitle">{channelTitle}</span>
+                        <span className="publishTime">{publishTime}</span>
                     </div>
-                    <p className="description">{this.props.description}</p>
+                    <p className="description">{description}</p>
                 </div>
             </div>
         )
